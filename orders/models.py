b32a24a2e868d616df4186d642cc9f2c9277cddb
jakhongirovmuslimbek <jmuslimbek2001@gmail.com>
2023-12-02 14:41:34 +0500

    commit

--- orders/models.py
+++ orders/models.py
@@ -3,9 +3,7 @@ from users.models import Customer
 from products.models import Product
 
 class Card(models.Model):
-    customer = models.ForeignKey(Customer, related_name="customer", on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, related_name="products")
-    
     created_date = models.DateTimeField(auto_now_add=True)
 
 
